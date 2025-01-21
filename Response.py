ce0f2cba8f64f137c7f2a5a819e69ae18ed4ca39
Akatsuki <handuy099@gmail.com>
2025-01-21 19:25:11 +0700

    testing

--- Response.py
+++ Response.py
@@ -1,6 +1,6 @@
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-from telegram.ext._utils.types import HandlerCallback
+from telegram.ext import ContextTypes
+from fuzzywuzzy import fuzz
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.message and update.effective_user:
@@ -8,16 +8,28 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
     else:
         print("Lỗi rồi không khởi động được, thử lại sau!")
 
-def handle_response(text: str) -> str:
+def handle_response(text: str, update) -> str:
     processed: str = text.lower()
-    if 'hello' in text: 
-        return 'Chào'
+    
+    def message(s, ans):
+        if fuzz.partial_ratio(processed, s) > 70 and update.effective_user:
+            return ans
+        return None
+    responses = [
+        ("xin chào", f"Xin chào {update.effective_user.first_name}")
+    ]
 
-    return 'Tôi không hiểu?'
+    for s, ans in responses:
+        res = message(s, ans)
+        if res:
+            return res
+    
+    return "Tôi không hiểu"
+
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message and update.message.text:
         user_message = update.message.text
-        response = handle_response(user_message)
+        response = handle_response(user_message, update)
         await update.message.reply_text(response)
 
